b63713ee5b47cc046a4728207ca21ed73276f80e
Kirit Sælensminde <k@kirit.com>
2015-05-11 22:44:42 +0700

    Create an initial process.

---
+++ rask/src/rask/main.cpp
@@ -0,0 +1,14 @@
+/*
+    Copyright 2015, Proteus Tech Co Ltd. http://www.kirit.com/Rask
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#include <fost/main>
+
+
+FSL_MAIN("rask", "Rask")(fostlib::ostream &out, fostlib::arguments &args) {
+    return 0;
+}
