4da8dc0d05f02896ade02c890537d19ce118da58
Kirit Sælensminde <k@kirit.com>
2015-05-24 14:23:12 +0700

    Allow us to load multiple log files.

--- rask/src/rask/main.cpp
+++ rask/src/rask/main.cpp
@@ -33,13 +33,24 @@ namespace {
 
 
 FSL_MAIN("rask", "Rask")(fostlib::ostream &out, fostlib::arguments &args) {
+    // Load the configuration files we've been given on the command line
+    std::vector<fostlib::settings> configuration;
+    configuration.reserve(args.size());
+    for ( std::size_t arg{1}; arg != args.size(); ++arg ) {
+        auto filename = fostlib::coerce<boost::filesystem::path>(args[arg].value());
+        out << "Loading config " << filename << std::endl;
+        configuration.emplace_back(filename);
+    }
+    // Set up the logging options
     std::unique_ptr<fostlib::log::global_sink_configuration> loggers;
     if ( !c_logger.value().isnull() && c_logger.value().has_key("sinks") ) {
         loggers = std::make_unique<fostlib::log::global_sink_configuration>(c_logger.value());
     }
+    // Start the threads for doing work
     rask::pool io(4), hashers(2);
     fostlib::log::debug("Started Rask");
+    // Spin up the web server
     fostlib::http::server server(fostlib::host(), 4000);
-    server(webserver);
+    server(webserver); // This will never return
     return 0;
 }
