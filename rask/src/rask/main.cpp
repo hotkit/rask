5dfb80e33d9d6a5b6ea978a1e648c784a690a85a
Kirit Sælensminde <k@kirit.com>
2015-05-24 11:50:36 +0700

    Add a logger and a log configuration.

--- rask/src/rask/main.cpp
+++ rask/src/rask/main.cpp
@@ -6,12 +6,24 @@
 */
 
 
+#include <fost/log>
 #include <fost/main>
 
 #include <rask/pool.hpp>
 
 
+namespace {
+    const fostlib::setting<fostlib::json> c_logger(
+        "rask/main.cpp", "rask", "logging", fostlib::null, true);
+}
+
+
 FSL_MAIN("rask", "Rask")(fostlib::ostream &out, fostlib::arguments &args) {
-    rask::pool sockets(4);
+    std::unique_ptr<fostlib::log::global_sink_configuration> loggers;
+    if ( !c_logger.value().isnull() && c_logger.value().has_key("sinks") ) {
+        loggers = std::make_unique<fostlib::log::global_sink_configuration>(c_logger.value());
+    }
+    rask::pool io(4), hashers(2);
+    fostlib::log::debug("Started Rask");
     return 0;
 }
